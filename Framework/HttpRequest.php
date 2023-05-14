46870fad7f2ed723844b441b095c3131c7be2c8a
marciv <barlot.marc@gmail.com>
2023-05-15 00:19:14 +0200

    Update HttpRequest.php

--- Framework/HttpRequest.php
+++ Framework/HttpRequest.php
@@ -40,7 +40,7 @@ class HttpRequest
 
     public function setPath($path)
     {
-        $this->request->pathInfo = "/".trim($path);
+        $this->request->pathInfo = "/".trim($path,"/");
     }    
 
     public function getMethod()
